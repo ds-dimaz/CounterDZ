2291d4e0081dc21b3f32b82a4b62a842d484a286
ds-dimaz <dimaz.pense@gmail.com>
2023-05-15 15:31:48 +0400

    Привёл приложение к требованиям базовой задачи Связал сториборд с кодом. Настроил работу кнопки и отображения значения счётчика на лейбле. Чуть-чуть доработал кнопку на сториборде (подвинул плюс внутри кнопки так, чтобы он отображался по центру)

--- CounterDZ/ViewController.swift
+++ CounterDZ/ViewController.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private var countInt = 0
+    @IBOutlet private weak var countLabel: UILabel!
+    @IBOutlet private weak var plusButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        countLabel.text = "0"
     }
-
+    
+    @IBAction private func plusButtonDidTap() {
+        countInt += 1
+        countLabel.text = String(countInt)
+    }
 
 }
 
